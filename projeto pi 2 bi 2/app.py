526bf10d5d2fa9818765d28d12775b0d7c3361d9
blackholeoficial641-art <blackholeoficial641@gmail.com>
2026-06-16 01:53:46 -0300

    Add files via upload

--- projeto pi 2 bi 2/app.py
+++ projeto pi 2 bi 2/app.py
@@ -1,13 +1,24 @@
 from flask import Flask
+
 from routes.auth import auth_bp
 from routes.projetos import projetos_bp
+from routes.versoes import versoes_bp
+from routes.github import github_bp
+from routes.drive import drive_bp
+from routes.imagens import imagens_bp
+from routes.solicitacoes import solicitacoes_bp
 
 app = Flask(__name__)
 
-app.config.from_object('config.Config')
+app.config.from_object("config.Config")
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(projetos_bp)
+app.register_blueprint(versoes_bp)
+app.register_blueprint(github_bp)
+app.register_blueprint(drive_bp)
+app.register_blueprint(imagens_bp)
+app.register_blueprint(solicitacoes_bp)
 
 @app.route("/")
 def home():
